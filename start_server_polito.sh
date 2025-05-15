704dd8206ead12fd3f869b6441f97a77f561f578
Federico Gorla <f.gorla@securitypattern.com>
2025-05-15 12:01:21 +0200

    Renamed polito certificate folder for clarity

--- start_server_polito.sh
+++ start_server_polito.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 # Launching server on 0.0.0.0 (!!)
-sudo ./installs/openssl/bin/openssl s_server  -CAfile certificates/testing_new/server/server-chain.pem  -cert certificates/testing_new/server/server.pem -key certificates/testing_new/server/server.key -groups X25519MLKEM768  -sigalgs mldsa44_ed25519  -accept 0.0.0.0:8884 -trace -verify 1 -verify_return_error
-
-# --cert_chain certificates/polito/int-ca.pem
-#
-#certificates/polito/int-ca.pem
+sudo ./installs/openssl/bin/openssl s_server  -CAfile certificates/polito/server/server-chain.pem  -cert certificates/polito/server/server.pem -key certificates/polito/server/server.key -groups X25519MLKEM768  -sigalgs mldsa44_ed25519  -accept 0.0.0.0:8884 -trace -verify 1 -verify_return_error
